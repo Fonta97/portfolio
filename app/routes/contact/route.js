8bc7be926fc372fb9da42c37773b89d0622826f5
Fonta97 <fontana.alessandro97@gmail.com>
2025-06-12 00:29:42 +0200

    fix css and font

--- app/routes/contact/route.js
+++ app/routes/contact/route.js
@@ -1,2 +1,2 @@
-export { default } from './contact';   // default export richiesto da Remix
+export { Contact as default } from './contact';   // default export richiesto da Remix
 export *       from './contact';       // re-esporta solo ciò che ESISTE (meta)
